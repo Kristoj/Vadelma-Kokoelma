48010c8cdda869a5628bcaf24b97ab2dfe7a8b01
ziibadaaba <jaan.johansson@gmail.com>
2019-04-19 20:14:25 +0300

    as

--- Saa_Asema/src/tallenna.py
+++ Saa_Asema/src/tallenna.py
@@ -3,6 +3,7 @@ import bme280_sample
 import matplotlib.pyplot as plt
 import time
 
+plt.ion()
 fig,(ax1,ax2,ax3) = plt.subplots(ncols=1,nrows=3)
 for ii in range(10):
 	bme280_sample.readData()
@@ -14,5 +15,6 @@ for ii in range(10):
 	#plt.show(block=False)
 	plt.draw()
 	
-	time.sleep(1)
+	plt.pause(1)
+	#time.sleep(1)
 	
