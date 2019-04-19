8b8e4b3f7c1f1296e36ea28f1f03dee629e3cd64
ziibadaaba <jaan.johansson@gmail.com>
2019-04-19 16:12:21 +0300

    viestikokeilu

--- Saa_Asema/src/tallenna.py
+++ Saa_Asema/src/tallenna.py
@@ -1,12 +1,14 @@
 
 import bme280_sample
 import matplotlib.pyplot as plt
+import time
 
 for ii in range(100):
 	bme280_sample.readData()
 	with open('temperatures.txt', 'r') as f:
 		t = f.readlines()
-		
+	
+	time.sleep(10)
 	#fig,(ax1,ax2,ax3) = plt.subplots(ncols=1,nrows=3)
 	#ax1.plot(t)
 	#ax1.set_title('Temperature')
