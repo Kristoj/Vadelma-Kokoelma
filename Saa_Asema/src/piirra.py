05e57603583df54db8b4bdeab07357e71347a4fd
ziibadaaba <jaan.johansson@gmail.com>
2019-04-20 07:28:35 +0300

    asd

--- Saa_Asema/src/piirra.py
+++ Saa_Asema/src/piirra.py
@@ -2,11 +2,9 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
-lognames = os.listdir('/home/pi/dev/projects/Vadelma-Kokoelma/Saa_Asema/data')
+lognames = os.listdir('../data')
 lastlog = np.sort(lognames)[-1]
-filename = '/home/pi/dev/projects/Vadelma-Kokoelma/Saa_Asema/data/'+lastlog
-
-fig,(ax1,ax2,ax3) = plt.subplots(ncols=1,nrows=3)
+filename = '../data/'+lastlog
 
 with open(filename, 'r') as f:
 	data = f.readlines()
@@ -15,21 +13,27 @@ hour = np.zeros(len(data))
 t = np.zeros(len(data))
 p = np.zeros(len(data))
 h = np.zeros(len(data))
+meas_time = np.zeros(len(data))
 for ii in range(len(data)):
 	hour[ii] = data[ii].split()[0]
 	t[ii] = data[ii].split()[2]
 	p[ii] = data[ii].split()[3]
 	h[ii] = data[ii].split()[4]
+	meas_time[ii] = float(data[ii].split()[0]+'.'+data[ii].split()[1])
 
-ax1.plot(hour,t)
+fig,(ax1,ax2,ax3) = plt.subplots(ncols=1,nrows=3)
+ax1.plot(meas_time,t)
 ax1.set_title('Temperature')
 ax1.set_xlim([0,24])
-ax2.plot(hour,p)
+ax1.set_ylabel('C')
+ax2.plot(meas_time,p)
 ax2.set_title('Pressure')
 ax2.set_xlim([0,24])
-ax3.plot(hour,h)
+ax2.set_ylabel('hPa')
+ax3.plot(meas_time,h)
 ax3.set_title('Humidity')
 ax3.set_xlim([0,24])
+ax3.set_ylabel('%')
 
 fig.tight_layout()
 plt.show()
